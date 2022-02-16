a40a950f72eecabcf58b60b1673d84dc146ff605
Abhinav Sharma <abhinav.161a026@gmail.com>
2022-02-16 21:51:51 +0530

    second-commit

--- controllers/expense.controller.js
+++ controllers/expense.controller.js
@@ -1,3 +1,4 @@
+const req = require("express/lib/request")
 const Expense = require("../models/expense.model")
 
 const getAllExpensesByUserId= async (req,res)=>{
@@ -39,7 +40,6 @@ const createAnExpense= async(req,res)=>{
                 employee_id:req.body.employee_id,
                 reimbursed:req.body.reimbursed
             })
-            console.log(expense)
         if(!expense){
             return res.status(400).json({msg:'Expense not created'})
         }
@@ -52,10 +52,46 @@ const createAnExpense= async(req,res)=>{
 }
 
 const getExpenseByDate =async (req,res)=>{
-    
+    try{
+        const per_page= req.query.per_page || 2
+        const page= req.query.page || 1
+        const skip= page<0?0:(page-1)*per_page
+        const expense= await Expense.find({$and:[{date_of_expense:{$gte:new Date(req.query.from)}},{date_of_expense:{$lte:new Date(req.query.to)}}]}).sort({date_of_expense:-1}).skip(skip).limit(per_page)
+        if(!expense){
+            return res.status(400).json({msg:'No expenses found'})
+        }
+        return res.status(200).json(expense)
+    }
+    catch(err){
+        return res.status(400).json({status:'failure',error:err.toString()})
+    }
+}
+
+const getExpenseByType = async (req,res)=>{
+    try{
+        const expense= await Expense.aggregate([{$group:{_id:'$type',count:{$sum:1}}},{$project : { Type_of_expenses : "$_id", "_id" : 0, No_of_Expenses : "$count" } }, {$sort:{No_of_Expenses: -1}}])
+        if(!expense){
+            return res.status(400).json({msg:'Something went wrong'})
+        }
+        return res.status(200).json(expense)
+    }
+    catch(err){
+        return res.status(400).json({status:'failure',error:err.toString()})
+    }
+}
+
+const getAverageTime = async (req,res)=>{
+    try{
+
+    }
+    catch(err){
+        
+    }
 }
 
 module.exports= {
     getAllExpensesByUserId,
-    createAnExpense
+    createAnExpense,
+    getExpenseByDate,
+    getExpenseByType
 }
--- routes/expense.route.js
+++ routes/expense.route.js
@@ -1,9 +1,13 @@
 const express= require('express')
-const { getAllExpensesByUserId, createAnExpense } = require('../controllers/expense.controller')
+const { getAllExpensesByUserId, createAnExpense, getExpenseByDate, getExpenseByType } = require('../controllers/expense.controller')
 const router= express.Router()
 
-router.get('/:user_id',getAllExpensesByUserId)
+router.get('/user:user_id',getAllExpensesByUserId)
+
+router.get('/dates',getExpenseByDate)
 
 router.post('/',createAnExpense)
+
+router.get('/type',getExpenseByType)
 
 module.exports=router
